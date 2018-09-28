2b94e5b94d4a27ce40f841fd93946389f488d702
Dominik <dominik.grybos@gmail.com>
2018-09-28 15:14:21 +0200

    buildInformation api has changed

--- packages/xdl/src/BuildInformation.js
+++ packages/xdl/src/BuildInformation.js
@@ -7,17 +7,15 @@ export async function getBuildInformation(options, projectDir) {
   const api = ApiV2Client.clientForUser(user);
   const { exp } = await ProjectUtils.readConfigJsonAsync(projectDir);
   const { id, platform, limit, status } = options;
-  const request = {
+  const params = {
     slug: exp.slug,
-    options: {
-      id,
-      platform: platform == null ? 'all' : platform,
-      limit,
-      status: status == null ? 'finished' : status,
-    },
+    id,
+    platform: platform == null ? 'all' : platform,
+    limit,
+    status: status == null ? 'finished' : status,
   };
   try {
-    const { builds } = await api.putAsync('build-information/get', request);
+    const { builds } = await api.getAsync('build-information/get', params);
     return builds;
   } catch (error) {
     return [];
